c12431f765438435f2cb1da8da77e91cf1349a40
yangqingren <564008993@qq.com>
2019-05-04 14:58:26 +0800

    0.1.9

--- Example/LBCocoaSpecKit/LBViewController.m
+++ Example/LBCocoaSpecKit/LBViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "LBViewController.h"
+#import <LBCocoaSpecKit/LBCocoaSpecKit.h>
 
 @interface LBViewController ()
 
@@ -17,6 +18,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
